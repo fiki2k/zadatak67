c9db9c1489913e2a0ac2d0fb60644abdbd4f4ccb
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-01-05 09:37:48 +0100

    Add files via upload

---
+++ zad67.c
@@ -0,0 +1,24 @@
+// Program ce izracunat volumen stozca.
+
+#include<stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+
+//Funkcija za izracunavanje volumena stozca.
+float volumen(int polumjer,int visina){
+	
+	float volumen;
+	volumen=(1.0/3.0)*PI*polumjer*polumjer*visina;
+	return volumen;
+}
+
+int main(){
+   int polumjer,visina;
+   printf("Unesi polumjer kugle:\n");
+   scanf("%d",&polumjer);
+   printf("Unesi visinu stozca:\n");
+   scanf("%d",&visina);
+   float V=volumen(polumjer,visina);
+   printf("Volumen stozca je %f\n",V);
+   return 0;
+}
